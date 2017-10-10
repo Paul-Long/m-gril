44ec0f520054bf63e5777472cd9d0854bfcb91f9
候宝龙 <houbaolong@niwodai.net>
2017-10-10 08:38:31 +0800

    <app-home> show photo

--- src/client/containers/Home/ShowPhoto.js
+++ src/client/containers/Home/ShowPhoto.js
@@ -1,10 +1,18 @@
 import React, {h} from 'react';
+import Image from 'components/Image';
 
 class ShowPhoto extends React.Component {
   render() {
     return (
       <div className='ym-show-photo'>
-
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
       </div>
     )
   }
